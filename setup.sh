d504ddd7e72d451a1c24c13344e30b1ddc1c3377
davidjung-kr <jerryjueng@daum.net>
2021-12-15 04:48:45 +0900

    셋업쉘 업데이트

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
+pip3 install pandas
 pip3 install konlpy
 pip3 install tweepy==3.10.0
-pip3 install pandas
+pip3 install matplotlib
+pip3 install seaborn
+pip3 install nltk
 pip3 install scikit-learn
-pip3 install tqdm
-pip3 install tensorflow
